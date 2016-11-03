d098ca18f77088878cbc95258bcfa192d4d46920
ankitjain28may <ankitjain28may77@gmail.com>
2016-11-03 21:35:52 +0530

    Added setup.py n update code n readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # FbPostUsingPython
-It is a small script to post status on your wall using python..!
+>It is a small script to post status on your FB wall using python..!
+
+#Installation
+1- Install python 3.5 from [Python.org](https://www.python.org)
+
+2- `sudo apt-get install python3-pip` (For Ubuntu Users)
+
+3- `python setup.py` or `python3 setup.py`
+
+  This will install all these packages
+
+  * `pip3 install bs4`
+
+  * `pip3 install selenium`
+
+After the installation, open terminal at the root folder--
+
+Run `python fb.py` or `python3 fb.py` to post status from terminal or cmd.
+
+Note:- Use Mozilla Firefox V40.0 to make this script work for you.
+
+##Some Useful Installation Tips ( Ubuntu )
+
+If you have already installed python 2.7 install python 3 as well but it may be the problem that the packages installed with respect to python 2.7 and shows error for the python 3 packages,
+
+So you need to install virtual Environment for the python 3 to install python3 packages.
+
+```
+    virtualenv -p /usr/bin/python3 py3env
+    source py3env/bin/activate
+    pip install package-name
+```
+
+After setting virtual environment install packages listed above and Enjoy.
+
+#License
+
+Copyright (c) 2016 Ankit Jain - Released under the Apache License
--- fb.py
+++ fb.py
@@ -2,63 +2,41 @@ import time
 import selenium
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import requests
-import urllib.request
-import json
-import validators
 from selenium.webdriver.common.keys import Keys
 
 
+def get_title(driver):
+    html_page = driver.page_source
+    soup = BeautifulSoup(html_page,'html.parser')
+    print(soup.title.string+"\n")
 
-def get_soup(url,header):
-    return BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url,headers=header)),'html.parser')
-
-
-header={'User-Agent':"Mozilla/6.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
-}
-
-print("Facebook")
 
 url = "https://facebook.com"
 
-print(url)
-
-
-
 driver = webdriver.Firefox()
 driver.get(url)
+time.sleep(2)
+get_title(driver)
 
-html_page = driver.page_source
-
-soup = BeautifulSoup(html_page,'html.parser')
-print(soup.title.string)
-
-eMail = input("Enter your email Id\n")
+eMail = input("Enter your Email Id or Mobile No. to Login\n")
 pAss = input("Enter your password\n")
 
 email = driver.find_element_by_name("email")
 email.send_keys(eMail)
-
 password = driver.find_element_by_name("pass")
 password.send_keys(pAss)
 
-
-
-start = driver.find_element_by_css_selector("#u_0_l")
-start.click()
-
+login = driver.find_element_by_css_selector("#u_0_l")
+login.click()
+print("You are logged in now\n")
 time.sleep(5)
+
+get_title(driver)
 pp = input("Enter your Status\n")
+status = driver.find_element_by_xpath('//textarea[@class="uiTextareaAutogrow _3en1" or @class="uiTextareaAutogrow _3en1 _480e"]')
+status.send_keys(pp)
 
-
-
-post = driver.find_element_by_xpath('//textarea[@class="uiTextareaAutogrow _3en1"]')
-post.send_keys(pp)
-
-NEXT_BUTTON_XPATH = '//button[@type="submit" and @class="_42ft _4jy0 _ej1 _4jy3 _4jy1 selected _51sy" and @value="1"]'
-button = driver.find_element_by_xpath(NEXT_BUTTON_XPATH)
+button = driver.find_element_by_xpath('//button[@type="submit" and @class="_42ft _4jy0 _ej1 _4jy3 _4jy1 selected _51sy" and @value="1"]')
 button.click()
-
-# Click_BUTTON_XPATH = '//form[@method="post" and @name="formNTQxNTQ1"]/input[2]'
-# _inputs = driver.find_element_by_xpath(Click_BUTTON_XPATH)
-# _inputs.click();
+print("Your Status : '"+pp+"' has been uploaded!\n")
+driver.quit()
